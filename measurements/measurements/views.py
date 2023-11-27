bf2534ea6a1c5fc60015d77d82520882269afa0f
dgomezrey <d.gomezr2@uniandes.edu.co>
2023-11-27 01:44:44 -0500

    qw

--- measurements/measurements/views.py
+++ measurements/measurements/views.py
@@ -33,11 +33,7 @@ def MeasurementCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-        if check_variable(data_json) == True:
-            return HttpResponse("variable correcta")
-        elif check_place(data_json) == True:
-            return HttpResponse("place correcta")
-        elif check_variable(data_json) == True and check_place(data_json) == True:
+        if check_variable(data_json) == True and check_place(data_json) == True:
             measurement = Measurement()
             measurement.variable = data_json['variable']
             measurement.value = data_json['value']
